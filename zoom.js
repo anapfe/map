7b76ea8f54b3ab48361386f08d9adbbf0dc6f2a8
Ana Pfefferkorn <anapfefferkorn@gmail.com>
2020-10-13 18:25:31 -0300

    limpiando el código

--- zoom.js
+++ zoom.js
@@ -72,38 +72,6 @@ window.addEventListener('load', function() {
             marker.style.left = (actualLeft - markerError) * percent + 'px';
           });
         }
-
-      } else {
-        // 
-        // percent = 1529.437 / 13000;
-        // var markerError = 0;
-        // for (var i = 0; i < 6; i++) {
-        //   error = error * 0.7;
-        //   markerError += error;
-        // }
-        //
-        // var imgCss = {};
-        // var dragCss = {};
-        //
-        // imgCss.width = currWidth * percent;
-        // var x = Math.abs(dragme.position().left) + container.width() / 2;
-        // var y = Math.abs(dragme.position().top) + container.height() / 2;
-        //
-        // var newX = x * percent;
-        // var newY = y * percent;
-        //
-        // dragCss.left = dragme.position().left - (newX - x);
-        // dragCss.top = dragme.position().top - (newY - y);
-        //
-        // mapImg.css(imgCss);
-        // dragme.css(dragCss);
-        //
-        // markers.forEach(function(marker) {
-        //   var actualTop = parseInt(marker.style.top, 10);
-        //   var actualLeft = parseInt(marker.style.left, 10);
-        //   marker.style.top = (actualTop - markerError) * percent + 'px';
-        //   marker.style.left = (actualLeft - markerError) * percent + 'px';
-        // });
       }
     });
   });
